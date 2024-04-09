725e9da3d31dbacb2c8af41e737fde9fd8eb55cd
Adea Gerguri <adea.gerguri@student.uni-pr.edu>
2024-04-09 02:41:37 +0200

    added morse code book and method instructions

---
+++ morse_code.py
@@ -0,0 +1,14 @@
+import time
+
+morse_code_fjalori = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
+                      'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
+                      'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
+                      'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+                      'Y': '-.--', 'Z': '--..', ' ': ' '}
+
+#me shtu nje metode qe merr textin si parameter e kthen si morse
+
+#me shtu nje metode qe merr morse_code si parameter e kthen si text
+
+#console beep metoda per me kthy si output me sound, textin e encodum
+#tu e importu nje librari per sound
