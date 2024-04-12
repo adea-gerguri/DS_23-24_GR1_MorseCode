ae2c159a896d12179b70ea0eeca97beb343ea74c
Adea Lluhani <131053382+adealluhani@users.noreply.github.com>
2024-04-12 23:49:28 +0200

    morse_code.py

--- morse_code.py
+++ morse_code.py
@@ -9,6 +9,16 @@ morse_code_fjalori = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
 #me shtu nje metode qe merr textin si parameter e kthen si morse
 
 #me shtu nje metode qe merr morse_code si parameter e kthen si text
+def morse_to_text(morse_code):
+    words = morse_code.split('  ')
+    text = ''
+    for word in words:
+        letters = word.split(' ')
+        for letter in letters:
+            if letter in morse_to_char_fjalori:
+                text += morse_to_char_fjalori[letter]
+        text += ' '
+    return text
 
 #console beep metoda per me kthy si output me sound, textin e encodum
 #tu e importu nje librari per sound
