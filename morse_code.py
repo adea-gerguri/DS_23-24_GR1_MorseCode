ed5f9f4417b97160d0e91e683f1d2935de3080b3
adeatabaku1 <122219494+adeatabaku1@users.noreply.github.com>
2024-04-13 15:34:18 +0200

    morse to char

--- morse_code.py
+++ morse_code.py
@@ -7,7 +7,15 @@ morse_code_fjalori = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
                       'Y': '-.--', 'Z': '--..', ' ': ' '}
 
 #me shtu nje metode qe merr textin si parameter e kthen si morse
+morse_to_char_fjalori = {char_morse: char_alfabet for char_alfabet, char_morse in morse_code_fjalori.items()}
 
+def text_to_morse(text):
+    morse_code = ''
+    for char in text.upper():
+        if char in morse_code_fjalori:
+            morse_code += morse_code_fjalori[char] + ' ' #me provu qetu me hek space per console.beep() se spi bon beep komplet
+    return morse_code
+  
 #me shtu nje metode qe merr morse_code si parameter e kthen si text
 def morse_to_text(morse_code):
     words = morse_code.split('  ')
