e595a45beab9731bc3203b1f2ea0f068213a2c0f
Adea Gerguri <adea.gerguri@student.uni-pr.edu>
2024-04-14 19:05:23 +0200

    added method for morse sound + final

--- morse_code.py
+++ morse_code.py
@@ -1,23 +1,23 @@
 import time
+import winsound
 
 morse_code_fjalori = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
                       'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
                       'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
                       'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
                       'Y': '-.--', 'Z': '--..', ' ': ' '}
-#sync
 
-#me shtu nje metode qe merr textin si parameter e kthen si morse
 morse_to_char_fjalori = {char_morse: char_alfabet for char_alfabet, char_morse in morse_code_fjalori.items()}
 
+#encode metoda
 def text_to_morse(text):
     morse_code = ''
     for char in text.upper():
         if char in morse_code_fjalori:
-            morse_code += morse_code_fjalori[char] + ' ' #me provu qetu me hek space per console.beep() se spi bon beep komplet
+            morse_code += morse_code_fjalori[char] + ' '
     return morse_code
   
-#me shtu nje metode qe merr morse_code si parameter e kthen si text
+#decode metoda
 def morse_to_text(morse_code):
     words = morse_code.split('  ')
     text = ''
@@ -29,5 +29,30 @@ def morse_to_text(morse_code):
         text += ' '
     return text
 
-#console beep metoda per me kthy si output me sound, textin e encodum
-#tu e importu nje librari per sound
+#metoda per beeps, e implementume me librarin winsound
+def console_beep(text):
+    for char in text.upper():
+        if char == ' ':
+            time.sleep(2)
+        else:
+            morse_char = morse_code_fjalori.get(char, '')
+            for symbol in morse_char:
+                if symbol == '.':
+                    winsound.Beep(600, 300)
+                if symbol == '-':
+                    winsound.Beep(600, 600)
+                time.sleep(0.1)
+
+#input text ose morse, varesisht tani per encode dhe decode
+inputi = input("Jep inputin ne tekst ose ne morse:")
+
+if '.' in inputi or '-' in inputi:
+    decoded_text = morse_to_text(inputi)
+    print("Morse code: ", inputi)
+    print("Teksti: ", decoded_text)
+else:
+    decoded_text = text_to_morse(inputi)
+    print("Teksti: ", inputi)
+    print("Morse code: ", decoded_text)
+
+console_beep(inputi)
